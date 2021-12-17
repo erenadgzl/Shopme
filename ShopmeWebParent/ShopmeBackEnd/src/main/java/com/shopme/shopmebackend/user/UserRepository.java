83a86fbe7d9ff3595c2515551ee5b5116a91a3b1
Eren Adıgüzel <erenadiguzellll@gmail.com>
2021-12-18 00:35:36 +0300

    created shopme project

---
+++ ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/shopmebackend/user/UserRepository.java
@@ -0,0 +1,9 @@
+package com.shopme.shopmebackend.user;
+
+import com.shopme.shopmecommon.entity.User;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends CrudRepository<User, Integer> {
+}
---
+++ ShopmeWebParent/ShopmeBackEnd/src/test/java/com/shopme/shopmebackend/user/UserRepositoryTests.java
@@ -0,0 +1,61 @@
+package com.shopme.shopmebackend.user;
+
+
+import com.shopme.shopmecommon.entity.Role;
+import com.shopme.shopmecommon.entity.User;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.test.annotation.Rollback;
+
+import static org.assertj.core.api.Assertions.*;
+
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@Rollback(value = false)
+public class UserRepositoryTests {
+
+    @Autowired
+    private UserRepository repo;
+
+    @Autowired
+    private TestEntityManager entityManager;
+
+    @Test
+    public void testCreateUser() {
+        Role role = entityManager.find(Role.class, 1);
+        User user = new User("erenadiguzel966@gmail.com", "123456", "Eren", "Adıguzel");
+        user.addRole(role);
+
+        User savedUser = repo.save(user);
+        assertThat(savedUser.getId()).isGreaterThan(0);
+    }
+
+    @Test
+    public void testListAllUsers(){
+        Iterable<User> listUsers = repo.findAll();
+        listUsers.forEach(user -> System.out.println(user));
+    }
+
+    @Test
+    public void testGetUserById(){
+        User user = repo.findById(1).get();
+        System.out.println(user);
+        assertThat(user).isNotNull();
+    }
+
+    @Test
+    public void testUpdateUserDetails(){
+        User user = repo.findById(1).get();
+        user.setEmail("test@test.com");
+        repo.save(user);
+    }
+
+    @Test
+    public void testDeleteUserById(){
+        User user = repo.findById(1).get();
+        repo.delete(user);
+    }
+}
