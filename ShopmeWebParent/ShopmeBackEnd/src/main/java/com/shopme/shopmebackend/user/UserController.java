59badc2ed2938fc4525050b7ef145f8322399367
Eren Adıgüzel <erenadiguzellll@gmail.com>
2021-12-18 02:43:58 +0300

    added user create

--- ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/shopmebackend/user/UserController.java
+++ ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/shopmebackend/user/UserController.java
@@ -1,12 +1,15 @@
 package com.shopme.shopmebackend.user;
 
 
+import com.shopme.shopmecommon.entity.Role;
 import com.shopme.shopmecommon.entity.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
 
@@ -18,9 +21,25 @@ public class UserController {
     private UserService userService;
 
     @GetMapping
-    public String listAll(Model model){
-        List<User> userList = userService.listAll();
-        model.addAttribute("userList", userList);
+    public String listAll(Model model) {
+        List<User> users = userService.listAll();
+        model.addAttribute("userList", users);
         return "users";
+    }
+
+    @GetMapping("/new")
+    public String newUser(Model model) {
+        List<Role> roles = userService.listRoles();
+        User user = new User();
+        model.addAttribute("user", user);
+        model.addAttribute("roles", roles);
+        return "user_form";
+    }
+
+    @PostMapping("/save")
+    public String saveUser(User user, RedirectAttributes redirectAttributes) {
+        userService.save(user);
+        redirectAttributes.addFlashAttribute("message", "Success");
+        return "redirect:/users";
     }
 }
--- ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/shopmebackend/user/UserService.java
+++ ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/shopmebackend/user/UserService.java
@@ -1,5 +1,6 @@
 package com.shopme.shopmebackend.user;
 
+import com.shopme.shopmecommon.entity.Role;
 import com.shopme.shopmecommon.entity.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -11,9 +12,18 @@ public class UserService {
 
     @Autowired
     private UserRepository userRepository;
+    @Autowired
+    private RoleRepository roleRepository;
 
-    public List<User> listAll(){
+    public List<User> listAll() {
         return (List<User>) userRepository.findAll();
     }
 
+    public List<Role> listRoles() {
+        return (List<Role>) roleRepository.findAll();
+    }
+
+    public void save(User user) {
+        userRepository.save(user);
+    }
 }
