a799d10b005b02e64d94863c8f8c9fbc20cd34e9
erenadiguzel <erenadiguzel96@gmail.com>
2022-01-19 23:34:29 +0300

    add shipping rate

---
+++ ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/shopmebackend/shippingrate/ShippingRateAlreadyExistsException.java
@@ -0,0 +1,9 @@
+package com.shopme.shopmebackend.shippingrate;
+
+public class ShippingRateAlreadyExistsException extends Exception {
+
+	public ShippingRateAlreadyExistsException(String message) {
+		super(message);
+	}
+
+}
