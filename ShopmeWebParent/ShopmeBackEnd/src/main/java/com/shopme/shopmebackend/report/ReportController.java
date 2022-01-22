2dde609c8ca559ebb86c9d0311ad5f6538b75654
erenadiguzel <erenadiguzel96@gmail.com>
2022-01-22 23:14:50 +0300

    add report

---
+++ ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/shopmebackend/report/ReportController.java
@@ -0,0 +1,30 @@
+package com.shopme.shopmebackend.report;
+
+import com.shopme.shopme.common.entity.setting.Setting;
+import com.shopme.shopmebackend.setting.SettingService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.List;
+
+@Controller
+public class ReportController {
+	
+	@Autowired private SettingService settingService;
+	
+	@GetMapping("/reports")
+	public String viewSalesReportHome(HttpServletRequest request) {
+		loadCurrencySetting(request);
+		return "reports/reports";
+	}
+	
+	private void loadCurrencySetting(HttpServletRequest request) {
+		List<Setting> currencySettings = settingService.getCurrencySettings();
+		
+		for (Setting setting : currencySettings) {
+			request.setAttribute(setting.getKey(), setting.getValue());
+		}	
+	}	
+}
---
+++ ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/shopmebackend/report/ReportType.java
@@ -0,0 +1,5 @@
+package com.shopme.shopmebackend.report;
+
+public enum ReportType {
+	DAY, MONTH, CATEGORY, PRODUCT
+}
